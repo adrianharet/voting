4845b45159674ff8f983b81979311b598de0a788
AH <4648823+adrian-haret@users.noreply.github.com>
2021-09-15 10:14:41 +0200

    Rewrote code, added functions for Kemeny.

--- voting.py
+++ voting.py
@@ -6,69 +6,57 @@ import matplotlib.pyplot as plt
 
 class Alternatives:
     def __init__(self, input):
-        if type(input) == int:
-            self.as_characters = list(string.ascii_lowercase)[:input]
-        elif type(input) == str:
-            self.as_characters = sorted(list(input))
-        elif isinstance(input, Iterable):
-            if all(type(c) == str for c in input):
-                self.as_characters = sorted(input)
-            elif all(type(i) == int for i in input):
-                self.as_characters = list(string.ascii_lowercase)[:len(input)]            
-        self.as_integers = list(range(len(self.as_characters)))
-        self.to_int_dict = {c: self.as_characters.index(c) for c in self.as_characters}
-        self.to_str_dict = {i:c for c, i in self.to_int_dict.items()}
-        self.number = len(self.as_integers)
+        """
+            Should be given as list of characters.
+        """
+        self.list = sorted(input)
 
     def __str__(self) -> str:
-        return str(self.as_characters)
+        return str(self.list)
+    
+    def __iter__(self):
+        return iter(self.list)
+
+    def number(self):
+        return len(self.list)
 
 class LinearOrder:
     def __init__(self, input):
-        self.alternatives = Alternatives(input)
+        if type(input) == list:
+            self.order = input
         if type(input) == str:
-            self.as_str = input
-            self.as_int_list = [self.alternatives.to_int_dict[c] for c in input]
-        elif isinstance(input, Iterable):
-            if all([type(c) == str for c in input]):
-                self.as_str = ''.join(input)
-                self.as_int_list = [self.alternatives.to_int_dict[c] for c in input]
-            if all([type(i) == int for i in input]):
-                self.as_int_list = input 
-                self.as_str = ''.join([self.alternatives.to_str_dict[i] for i in input])
+            self.order = list(input)
 
     def __str__(self) -> str:
-        return self.as_str
+        return ''.join(self.order)
     
     def length(self):
-        return len(self.as_int_list)
+        return len(self.order)
 
     def edges(self):
-        return list(itertools.combinations(self.as_int_list, 2))
+        return list(itertools.combinations(self.order, 2))
 
     def index(self, x):
-        if isinstance(x, int):
-            return self.as_int_list.index(x)
-        if isinstance(x, str):
-            return self.as_str.index(x)
+        return self.order.index(x)
 
 class MajorityGraph:
-    def __init__(self, nodes, edge_list = [], weighted_edge_list = []):
+    def __init__(self, nodes, edge_list = []):
         self.G = nx.DiGraph()
-        self.alternatives = Alternatives(nodes)
-        self.G.add_nodes_from(self.alternatives.as_integers)
-        if edge_list and all([type(e) == str for e in edge_list]):
-            self.G.add_edges_from([(self.alternatives.to_int_dict[e[0]], self.alternatives.to_int_dict[e[1]]) for e in edge_list])
-        self.G.add_weighted_edges_from(weighted_edge_list)
+        self.weighted = all(len(e) == 3 for e in edge_list) 
+        self.G.add_nodes_from(nodes)
+        if self.weighted:
+            self.G.add_weighted_edges_from(edge_list)
+        else:
+            self.G.add_weighted_edges_from([(e[0], e[1], 1) for e in edge_list])
     
     def add_nodes_from(self, input):
         self.G.add_nodes_from(input)
     
     def add_edge(self, u, v, weight=1):
-        if type(u) == str and type(v) == str:
-            self.G.add_edge(self.alternatives.to_int_dict[u], self.alternatives.to_int_dict[v], weight = weight)
-        if type(u) == int and type(v) == int:
-            self.G.add_edge(u, v, weight = weight)
+        self.G.add_edge(u, v, weight = weight)
+    
+    def update_edge_weight(self, u, v, w):
+        self.G[u][v]['weight'] = w
         
     def nodes(self):
         return self.G.nodes
@@ -76,17 +64,20 @@ class MajorityGraph:
     def edges(self):
         return self.G.edges
     
-    def draw(self):
-        pos = nx.kamada_kawai_layout(self.G)
-        nx.draw(self.G, pos, labels = self.alternatives.to_str_dict, node_color='white', font_size=16, font_family='serif', node_size=1000, edgecolors = 'black')
-        labels = nx.get_edge_attributes(self.G, 'weight')
-        nx.draw_networkx_edge_labels(self.G, pos, edge_labels = labels)
-        plt.show()
+    def weighted_edges(self):
+        return nx.get_edge_attributes(self.G, 'weight')
 
+    def draw(self):
+        pos = nx.spring_layout(self.G)
+        nx.draw(self.G, pos, node_color='white', with_labels=True, font_size=16, font_family='serif', node_size=1000, edgecolors = 'black')
+        if self.weighted:
+            labels = nx.get_edge_attributes(self.G, 'weight')
+            nx.draw_networkx_edge_labels(self.G, pos, edge_labels=labels)  
+        plt.show()
 class Profile:
-    def __init__(self, orders):
-        self.orders = [LinearOrder(order) for order in orders]
-        self.alternatives = self.orders[0].alternatives
+    def __init__(self, input):
+        self.orders = [LinearOrder(order) for order in input]
+        self.alternatives = Alternatives(input[0])
 
     def __str__(self):
         return str([str(L) for L in self.orders])
@@ -117,8 +108,8 @@ class Profile:
         return self.support(x, y) - self.support(y, x)
 
     def majority_graph(self):
-        G = MajorityGraph(nodes = self.alternatives.as_integers)
-        for (x, y) in itertools.combinations(self.alternatives.as_integers, 2): # add the edges that enjoy majority support
+        G = MajorityGraph(nodes = self.alternatives)
+        for (x, y) in itertools.combinations(self.alternatives, 2): # add the edges that enjoy majority support
             w = self.margin(x,y)
             if w > 0:
                 G.add_edge(x, y, weight = w)
@@ -132,8 +123,7 @@ class Generator:
     
     def all_linear_orders(self, alternatives):
         for S in itertools.permutations(alternatives, len(alternatives)):
-            yield LinearOrder(list(S))                
-
+            yield LinearOrder(list(S))       
 class Distance:
     def __init__(self) -> None:
         pass
@@ -148,7 +138,17 @@ class Distance:
             [(1,2), (2,3), (1,3)], [(3,2), (2,1), (3,1)] --> 6
         """
         return len(set(l1) ^ set(l2)) # the length of the symmetric difference of L1 and L2, thought of as sets
-
+    
+    def weightedHamming(self, G, L):
+        """
+            Assumes G is a MajorityGraph and L is a LinearOrder.
+            Computes distance based on number of edges in G that l inverts.
+        """
+        d = 0
+        for (x, y) in L.edges():
+            if (y, x) in G.edges():
+                d += G.weighted_edges()[(y, x)]
+        return d
 class VotingRules:
     def __init__(self, input):
         if type(input) == MajorityGraph:
@@ -156,17 +156,34 @@ class VotingRules:
         if type(input) == Profile:
             self.G = input.majority_graph()
         self.Slater_result = {}
+        self.Kemeny_result = {}
         self.Slater_minimal_score = float('inf')
+        self.Kemeny_minimal_score = float('inf')
 
-    def compute_Slater_result(self):
-        nodes, edges = self.G.nodes(), self.G.edges()
-        for l in Generator().all_linear_orders(nodes):
-            current_score = Distance().Hamming(edges, l.edges())
-            if current_score < self.Slater_minimal_score:
-                self.Slater_result = {l}
-                self.Slater_minimal_score = current_score
-            elif self.Slater_minimal_score == current_score:
-                self.Slater_result = self.Slater_result | {l}
+    def closest_to_graph(self, G):
+        """
+            Assumes G is a MajorityGraph.
+        """
+        result = {}
+        minimal_score = float('inf')
+        for L in Generator().all_linear_orders(G.nodes()):
+            current_score = Distance().weightedHamming(G, L)
+            if current_score < minimal_score:
+                result = {L}
+                minimal_score = current_score
+            elif current_score == minimal_score:
+                result = result | {L}
+        return (result, minimal_score)
     
+    def compute_Kemeny_result(self):
+        self.Kemeny_result, self.Kemeny_minimal_score = self.closest_to_graph(self.G)[0], self.closest_to_graph(self.G)[1] 
+    
+    def compute_Slater_result(self):
+        unweightedG = MajorityGraph(nodes=self.G.nodes(), edge_list=self.G.edges())
+        self.Slater_result, self.Slater_minimal_score = self.closest_to_graph(unweightedG)[0], self.closest_to_graph(unweightedG)[1] 
+
     def show_Slater_result(self) -> str:
         print("{" + str(sorted([str(l) for l in self.Slater_result]))[1:-1] + "}")
+
+    def show_Kemeny_result(self) -> str:
+        print("{" + str(sorted([str(l) for l in self.Kemeny_result]))[1:-1] + "}")
